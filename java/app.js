6a3f2e995f5da27c81a0c4d936a6cc5348ae563b
areyes2853 <46231827+areyes2853@users.noreply.github.com>
2025-03-11 22:30:51 -0400

    update

--- java/app.js
+++ java/app.js
@@ -2,3 +2,9 @@ const handBurger = document.querySelector('.mobile-menu');
 handBurger.addEventListener("click",()=>{
     document.querySelector('.menu').classList.toggle('show')
 })
+
+// Select all "like" buttons
+const likeButtons = document.querySelectorAll('.like-btn');
+
+// Select all "dislike" buttons
+const dislikeButtons = document.querySelectorAll('.dislike-btn');
